b71c85cb0c38826870906e744f52186f2d9ebfc3
Benjamin Arratia <benjamin.arratia.santibanez@gmail.com>
2023-01-21 21:55:09 -0300

    Compilacion con Gulp y sus tareas, @import-@use

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+//src identifica el archivo, dest lo guarda.
+const { src, dest, watch } = require('gulp'); //Extraer funcionalidades de una dependencia.
+const sass = require('gulp-sass')(require('sass'));
+
+function css( cb ) {
+    src('src/scss/**/*.scss') //Identificar el archivo SASS
+        .pipe( sass() ) //Compilarlo
+        .pipe( dest("build/css") ) //Guardarlo en el Disco Duro
+
+    cb(); //CallBack
+}
+
+function dev( cb ) {
+    watch('src/scss/**/*.scss', css);
+    cb();
+}
+
+exports.css = css;
+exports.dev = dev;
