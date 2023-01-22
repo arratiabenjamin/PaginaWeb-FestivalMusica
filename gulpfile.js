3e054eb9fd85a4075d3df8f6c469f91901faf0f8
Benjamin Arratia <benjamin.arratia.santibanez@gmail.com>
2023-01-21 22:25:02 -0300

    Comiezo de Proyecto - Creacion Variables y Header

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 //src identifica el archivo, dest lo guarda.
 const { src, dest, watch } = require('gulp'); //Extraer funcionalidades de una dependencia.
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( cb ) {
     src('src/scss/**/*.scss') //Identificar el archivo SASS
+        .pipe( plumber() )
         .pipe( sass() ) //Compilarlo
         .pipe( dest("build/css") ) //Guardarlo en el Disco Duro
 
